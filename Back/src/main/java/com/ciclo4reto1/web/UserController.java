c14b57190c31759cee7e6f1f678b3b346189aeaa
Saregom <santi26reyes@gmail.com>
2021-11-22 08:08:41 -0500

    Se corrigio controller

--- Back/src/main/java/com/ciclo4reto1/web/UserController.java
+++ Back/src/main/java/com/ciclo4reto1/web/UserController.java
@@ -15,29 +15,52 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Class UserController
+ * @author Santiago Reyes
+ */
 @RestController
-@RequestMapping("/api/User")
+@RequestMapping("/api/user")
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class UserController {
+    /**
+     * Atributo userS
+     */
     @Autowired
     private UserService userS;
     
+    /**
+     * @return userS.getAll();
+     */
     @GetMapping("/all")
     public List<User> getUsers(){
         return userS.getAll();
     }
    
+    /**
+     * @param user
+     * @return userS.save(user);
+     */
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public User save(@RequestBody User user){
         return userS.save(user);
     }
     
+    /**
+     * @param email
+     * @return userS.getEmail(email);
+     */
     @GetMapping("/{email}")
     public boolean getEmail(@PathVariable("email") String email){
         return userS.getEmail(email);
     }
     
+    /**
+     * @param email
+     * @param password
+     * @return userS.getVerifyUser(email, password);
+     */
     @GetMapping("/{email}/{password}")
     public User getVerifyUser(@PathVariable("email") String email, @PathVariable("password") String password){
         return userS.getVerifyUser(email, password);
